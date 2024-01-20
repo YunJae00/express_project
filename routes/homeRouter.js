d841567797b1780669719138e9486c619cd23d93
rnjsdbswo00 <72205683+rnjsdbswo00@users.noreply.github.com>
2024-01-21 00:32:25 +0900

    aaa

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -5,50 +5,50 @@ module.exports = function (passport) {
     var router = require('express').Router();
     let request = require('request');
 
-    router.get('/main', function(req, res){
-        res.render('main.html');
-    });
+    // router.get('/main', function(req, res){
+    //     res.render('main.html');
+    // });
 
-    router.get('/login', function(req, res){
-        res.render('login.html');
-    });
+    // router.get('/login', function(req, res){
+    //     res.render('login.html');
+    // });
 
-    router.post('/login', function(req, res){
-        passport.authenticate(
-            'local',
-            {
-                successRedirect: '/dashboard',
-                failureRedirect: '/main',
-                failureFlash: false
-            }
-        )
-    });
+    // router.post('/login', function(req, res){
+    //     passport.authenticate(
+    //         'local',
+    //         {
+    //             successRedirect: '/dashboard',
+    //             failureRedirect: '/main',
+    //             failureFlash: false
+    //         }
+    //     )
+    // });
 
-    router.post('/register', function(req, res){
-        hasher({ password: req.body.password }, function (err, pass, salt, hash) {
-            var user = {
-                userId : req.body.username
-                //id: req.body.id,
-                //email: req.body.email,
-                ,password: hash,
-                salt: salt,
-            };
-            var sql = 'INSERT INTO users SET ?';
-            conn.query(sql, user, function (err, results) {
-                if (err) {
-                    console.log(err);
-                    res.status(500);
-                }
-                else {
-                    res.redirect('/login');
-                }
-            });
-        });
-    });
+    // router.post('/register', function(req, res){
+    //     hasher({ password: req.body.password }, function (err, pass, salt, hash) {
+    //         var user = {
+    //             userId : req.body.username
+    //             //id: req.body.id,
+    //             //email: req.body.email,
+    //             ,password: hash,
+    //             salt: salt,
+    //         };
+    //         var sql = 'INSERT INTO users SET ?';
+    //         conn.query(sql, user, function (err, results) {
+    //             if (err) {
+    //                 console.log(err);
+    //                 res.status(500);
+    //             }
+    //             else {
+    //                 res.redirect('/login');
+    //             }
+    //         });
+    //     });
+    // });
 
-    router.get('/dashboard', function(req, res) {
-        res.render('dashboard', { user: req.user });
-    });
+    // router.get('/dashboard', function(req, res) {
+    //     res.render('dashboard', { user: req.user });
+    // });
 
     return router;
 }
